7bae3061c3812c9bdd1a34f60e0ab96a668336ac
Margaret Gardner <mgardner457@gmail.com>
2023-10-02 13:47:08 -0400

    correct each measure type (vol, sa, ct) by different global measure

--- R_scripts/fit_ukb_mod.R
+++ R_scripts/fit_ukb_mod.R
@@ -11,17 +11,20 @@ library(data.table)
 args <- commandArgs(trailingOnly = TRUE)
 ukb_df <- fread(args[1], stringsAsFactors = TRUE, na.strings = "")
 pheno <- as.character(args[2])
-save_path <- args[3]
+glob_pheno <- as.character(args[3])
+save_path <- args[4]
 
 #FIT BASE MODEL
-base_model <- gamlss(formula = as.formula(paste0(pheno,"~ pb(age_days) + sex + fs_version + pb(sex.age) + pb(TBV)")),
+base_model <- gamlss(formula = as.formula(paste0(pheno,"~ pb(age_days) + sex + fs_version + pb(sex.age) + pb(", glob_pheno,")")),
              sigma.formula = ~  pb(age_days) + sex,
              nu.formula = ~ 1,
              control = gamlss.control(n.cyc = 200), 
              family = BCCG, data=ukb_df, trace = FALSE)
 
 #USE stepGAICAll.A to fit best model
-best_model <- stepGAICAll.A(base_model, scope=list(lower=~1, upper=~pb(age_days) + sex + fs_version + pb(sex.age) + pb(TBV)), k=2, glim.control(cyc=100)) #AIC
+best_model <- stepGAICAll.A(base_model, 
+                            scope=list(lower=~1, upper=as.formula(paste0("~ pb(age_days) + sex + fs_version + pb(sex.age) + pb(", glob_pheno,")"))), 
+                            k=2, glim.control(cyc=100)) #AIC
 
 #SAVE
 saveRDS(best_model,paste0(save_path, "/", pheno, "_mod.rds"))
--- qsub_gamlss.sh
+++ qsub_gamlss.sh
@@ -4,10 +4,11 @@
 
 #######################################################################################
 # SET PATHS
-base=/cbica/home/gardnerm/combat-biovar #base path
+base=/cbica/home/gardnerm/combat-biovar #base path (cubic)
+#base=/Users/megardn/Desktop/BGD_Repos/combat_biovar #base path (local)
 data_csv=$base/data/ukb_CN_data.csv #path to csv with CLEANED DATA (no duplicates, etc)
-pheno_list=$base/phenotype_list.txt #path to .txt file listing phenotypes (global & regional)
-mod_script=$base/fit_ukb_mod.R #path to .R script
+pheno_path=$base/pheno_lists #path to .txt files listing phenotypes (global & regional)
+mod_script=$base/R_scripts/fit_ukb_mod.R #path to .R script
 
 #cd to where .Rprofile is stored (for propper package loading)
 cd $base
@@ -39,15 +40,81 @@ if ! [ -d $gamlss_dir ]
 #######################################################################################
 # FIT MODELS
 
-#iterate through list of phenotypes
-while read -r pheno
+#iterate through measure types (vol, SA, CT, global vols) for correct global corrections
+for list in "$pheno_path"/*
 do
-    #write bash script
-	bash_script=$bash_dir/${pheno}_fit.sh
-	touch $bash_script
-	echo "Rscript --save $mod_script $data_csv $pheno $gamlss_dir" > $bash_script
+	if [ $list = $pheno_path/"Vol_list_global.txt" ]
+	then
+		glob_pheno="TBV"
+		echo "Submitting models for global volumes corrected by $glob_pheno"
 
-	#qsub bash script
-	qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+		#iterate through list of phenotypes
+		while read -r pheno
+		do
+			#write bash script
+			bash_script=$bash_dir/${pheno}_fit.sh
+			touch $bash_script
+			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
 
-done < $pheno_list
+			#qsub bash script
+			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+
+		done < $list
+
+	elif [ $list = $pheno_path/"Vol_list_regions.txt" ]
+	then
+		glob_pheno="Vol_total"
+		echo "Submitting models for regional volumes corrected by $glob_pheno"
+
+		#iterate through list of phenotypes
+		while read -r pheno
+		do
+			#write bash script
+			bash_script=$bash_dir/${pheno}_fit.sh
+			touch $bash_script
+			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+
+			#qsub bash script
+			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+
+		done < $list
+
+	elif [ $list = $pheno_path/"SA_list.txt" ]
+	then
+		glob_pheno="SA_total"
+		echo "Submitting models for regional SA corrected by $glob_pheno"
+
+		#iterate through list of phenotypes
+		while read -r pheno
+		do
+			#write bash script
+			bash_script=$bash_dir/${pheno}_fit.sh
+			touch $bash_script
+			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+
+			#qsub bash script
+			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+
+		done < $list
+
+	elif [ $list = $pheno_path/"CT_list.txt" ]
+	then
+		glob_pheno="CT_total"
+		echo "Submitting models for regional CT corrected by $glob_pheno"
+
+		#iterate through list of phenotypes
+		while read -r pheno
+		do
+			#write bash script
+			bash_script=$bash_dir/${pheno}_fit.sh
+			touch $bash_script
+			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+
+			#qsub bash script
+			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+
+		done < $list
+	else
+		echo "Help! I don't know what to do with this list."
+	fi
+done
