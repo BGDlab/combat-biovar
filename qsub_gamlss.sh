3c0b409752fa4cef7fc3dd0558863ab8dd900577
Margaret Gardner <mgardner457@gmail.com>
2023-10-18 17:45:00 -0400

    iterate through csv or dir

--- qsub_gamlss.sh
+++ qsub_gamlss.sh
@@ -6,14 +6,18 @@
 # SET PATHS
 base=/cbica/home/gardnerm/combat-biovar #base path (cubic)
 #base=/Users/megardn/Desktop/BGD_Repos/combat_biovar #base path (local)
-data_csv=$base/data/ukb_CN_data.csv #path to csv with CLEANED DATA (no duplicates, etc)
+#data_csv=$base/data/ukb_CN_data.csv #path to csv with CLEANED DATA (no duplicates, etc)
 pheno_path=$base/pheno_lists #path to .txt files listing phenotypes (global & regional)
 mod_script=$base/R_scripts/fit_ukb_mod.R #path to .R script
-
-#cd to where .Rprofile is stored (for propper package loading)
-cd $base
 #######################################################################################
-
+#######################################################################################
+# GET ARGS
+if [ $# -lt 1 ]
+then
+	echo 1>&2 "$0: provide path to data (either single .csv or dir)"
+	exit 2
+fi
+#######################################################################################
 # MAKE DIRECTORIES 
 
 #study dir
@@ -39,82 +43,174 @@ if ! [ -d $gamlss_dir ]
 
 #######################################################################################
 # FIT MODELS
+#if input .csv
+if [ -f $1 ]
+then
+	csv_name=$(basename $1 .csv)
+	csv_name=${csv_name//_/\-}
+	#iterate through measure types (vol, SA, CT, global vols) for correct global corrections
+	for list in "$pheno_path"/*
+	do
+		if [ $list = $pheno_path/"Vol_list_global.txt" ]
+		then
+			glob_pheno="TBV"
+			echo "Submitting models for global volumes corrected by $glob_pheno"
 
-#iterate through measure types (vol, SA, CT, global vols) for correct global corrections
-for list in "$pheno_path"/*
-do
-	if [ $list = $pheno_path/"Vol_list_global.txt" ]
-	then
-		glob_pheno="TBV"
-		echo "Submitting models for global volumes corrected by $glob_pheno"
+			#iterate through list of phenotypes
+			while read -r pheno
+			do
+				#write bash script
+				bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+				touch $bash_script
+				echo "Rscript --save $mod_script $1 $pheno $glob_pheno $gamlss_dir" > $bash_script
 
-		#iterate through list of phenotypes
-		while read -r pheno
+				#qsub bash script
+				qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
+
+			done < $list
+
+		elif [ $list = $pheno_path/"Vol_list_regions.txt" ]
+		then
+			glob_pheno="Vol_total"
+			echo "Submitting models for regional volumes corrected by $glob_pheno"
+
+			#iterate through list of phenotypes
+			while read -r pheno
+			do
+				#write bash script
+				bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+				touch $bash_script
+				echo "Rscript --save $mod_script $1 $pheno $glob_pheno $gamlss_dir" > $bash_script
+
+				#qsub bash script
+				qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
+
+			done < $list
+
+		elif [ $list = $pheno_path/"SA_list.txt" ]
+		then
+			glob_pheno="SA_total"
+			echo "Submitting models for regional SA corrected by $glob_pheno"
+
+			#iterate through list of phenotypes
+			while read -r pheno
+			do
+				#write bash script
+				bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+				touch $bash_script
+				echo "Rscript --save $mod_script $1 $pheno $glob_pheno $gamlss_dir" > $bash_script
+
+				#qsub bash script
+				qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
+
+			done < $list
+
+		elif [ $list = $pheno_path/"CT_list.txt" ]
+		then
+			glob_pheno="CT_total"
+			echo "Submitting models for regional CT corrected by $glob_pheno"
+
+			#iterate through list of phenotypes
+			while read -r pheno
+			do
+				#write bash script
+				bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+				touch $bash_script
+				echo "Rscript --save $mod_script $1 $pheno $glob_pheno $gamlss_dir" > $bash_script
+
+				#qsub bash script
+				qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
+
+			done < $list
+		else
+			echo "Help! I don't know what to do with this list."
+		fi
+	done
+#if input directory of .csvs
+elif [ -d $1 ]
+then
+	for csv_file in "$1"/*
+	do
+		csv_name=$(basename $csv_file .csv)
+		csv_name=${csv_name//_/\-}
+		echo "Pulling data from $csv_name"
+		#iterate through measure types (vol, SA, CT, global vols) for correct global corrections
+		for list in "$pheno_path"/*
 		do
-			#write bash script
-			bash_script=$bash_dir/${pheno}_fit.sh
-			touch $bash_script
-			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+			if [ $list = $pheno_path/"Vol_list_global.txt" ]
+			then
+				glob_pheno="TBV"
+				echo "Submitting models for global volumes corrected by $glob_pheno"
 
-			#qsub bash script
-			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+				#iterate through list of phenotypes
+				while read -r pheno
+				do
+					#write bash script
+					bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+					touch $bash_script
+					echo "Rscript --save $mod_script $csv_file $pheno $glob_pheno $gamlss_dir" > $bash_script
 
-		done < $list
+					#qsub bash script
+					qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
 
-	elif [ $list = $pheno_path/"Vol_list_regions.txt" ]
-	then
-		glob_pheno="Vol_total"
-		echo "Submitting models for regional volumes corrected by $glob_pheno"
+				done < $list
 
-		#iterate through list of phenotypes
-		while read -r pheno
-		do
-			#write bash script
-			bash_script=$bash_dir/${pheno}_fit.sh
-			touch $bash_script
-			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+			elif [ $list = $pheno_path/"Vol_list_regions.txt" ]
+			then
+				glob_pheno="Vol_total"
+				echo "Submitting models for regional volumes corrected by $glob_pheno"
 
-			#qsub bash script
-			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+				#iterate through list of phenotypes
+				while read -r pheno
+				do
+					#write bash script
+					bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+					touch $bash_script
+					echo "Rscript --save $mod_script $csv_file $pheno $glob_pheno $gamlss_dir" > $bash_script
 
-		done < $list
+					#qsub bash script
+					qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
 
-	elif [ $list = $pheno_path/"SA_list.txt" ]
-	then
-		glob_pheno="SA_total"
-		echo "Submitting models for regional SA corrected by $glob_pheno"
+				done < $list
 
-		#iterate through list of phenotypes
-		while read -r pheno
-		do
-			#write bash script
-			bash_script=$bash_dir/${pheno}_fit.sh
-			touch $bash_script
-			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+			elif [ $list = $pheno_path/"SA_list.txt" ]
+			then
+				glob_pheno="SA_total"
+				echo "Submitting models for regional SA corrected by $glob_pheno"
 
-			#qsub bash script
-			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+				#iterate through list of phenotypes
+				while read -r pheno
+				do
+					#write bash script
+					bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+					touch $bash_script
+					echo "Rscript --save $mod_script $csv_file $pheno $glob_pheno $gamlss_dir" > $bash_script
 
-		done < $list
+					#qsub bash script
+					qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
 
-	elif [ $list = $pheno_path/"CT_list.txt" ]
-	then
-		glob_pheno="CT_total"
-		echo "Submitting models for regional CT corrected by $glob_pheno"
+				done < $list
 
-		#iterate through list of phenotypes
-		while read -r pheno
-		do
-			#write bash script
-			bash_script=$bash_dir/${pheno}_fit.sh
-			touch $bash_script
-			echo "Rscript --save $mod_script $data_csv $pheno $glob_pheno $gamlss_dir" > $bash_script
+			elif [ $list = $pheno_path/"CT_list.txt" ]
+			then
+				glob_pheno="CT_total"
+				echo "Submitting models for regional CT corrected by $glob_pheno"
 
-			#qsub bash script
-			qsub -N ${pheno} -o $bash_dir/${pheno}_out.txt -e $bash_dir/${pheno}_err.txt $bash_script
+				#iterate through list of phenotypes
+				while read -r pheno
+				do
+					#write bash script
+					bash_script=$bash_dir/${pheno}_${csv_name}_fit.sh
+					touch $bash_script
+					echo "Rscript --save $mod_script $csv_file $pheno $glob_pheno $gamlss_dir" > $bash_script
 
-		done < $list
-	else
-		echo "Help! I don't know what to do with this list."
-	fi
-done
+					#qsub bash script
+					qsub -N ${pheno}.${csv_name} -o $bash_dir/${pheno}_${csv_name}_out.txt -e $bash_dir/${pheno}_${csv_name}_err.txt $bash_script
+
+				done < $list
+			else
+				echo "Help! I don't know what to do with this list."
+			fi
+		done
+	done
+fi
