801c3ec100245807f63948abe15ba91dcb049bfe
Margaret Gardner <mgardner457@gmail.com>
2023-10-19 10:39:12 -0400

    singularity run

--- var_parse.sh
+++ var_parse.sh
@@ -1,1 +1,4 @@
+singularity run --cleanenv \
+-B /cbica/home/gardnerm/combat-biovar/ukb_basic/gamlss_objs,/cbica/home/gardnerm/combat-biovar/R_scripts \
+/cbica/home/gardnerm/software/containers/r_gamlss_0.0.1.sif \
 Rscript --save /cbica/home/gardnerm/combat-biovar/R_scripts/variance_parser.R /cbica/home/gardnerm/combat-biovar/ukb_basic/gamlss_objs /cbica/home/gardnerm/combat-biovar/ukb_basic
