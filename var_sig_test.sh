4da9a95f18e2695e25a236b4b9744aec7c982c0f
Margaret Gardner <mgardner457@gmail.com>
2023-10-19 10:23:18 -0400

    deprecating in favor of varaince_parser.R

--- var_sig_test.sh
+++
@@ -1,4 +0,0 @@
-#cubic
-#Rscript --save /cbica/home/gardnerm/combat-biovar/R_scripts/sex_sig_test.R /cbica/home/gardnerm/combat-biovar/ukb_basic/gamlss_objs /cbica/home/gardnerm/combat-biovar/ukb_basic
-#local
-Rscript --save /Users/megardn/Desktop/BGD_Repos/combat_biovar/R_scripts/sex_sig_test.R /Users/megardn/Desktop/BGD_Repos/combat_biovar/ukb_basic/gamlss_objs /Users/megardn/Desktop/BGD_Repos/combat_biovar/ukb_basic
