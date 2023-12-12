7e0d41992d3dff41d23c1418aee0939e1dd414fd
Margaret Gardner Margaret.Gardner@Pennmedicine.upenn.edu <mgardner457@gmail.com>
2023-12-12 13:47:58 -0500

    updating to combat lifespan data

--- cubic_scripts/R_scripts/combat_apply.R
+++ cubic_scripts/R_scripts/combat_apply.R
@@ -102,7 +102,7 @@ for (l in list_of_feature_lists){
     dplyr::select(all_of(l))
   
   #log-transform global vals - added for lifespan data analyses, not sure if i should rerun for ukb analyses
-  if (names(list_of_feature_lists[i]) = "VolGlob"){
+  if (names(list_of_feature_lists[i]) == "VolGlob"){
     pheno.df <- pheno.df %>%
       mutate(across(c(l), \(x) log(x, base=10)))
   }
@@ -134,7 +134,7 @@ for (l in list_of_feature_lists){
   }
   
   #un-log-transform global vals
-  if (names(list_of_feature_lists[i]) = "VolGlob"){
+  if (names(list_of_feature_lists[i]) == "VolGlob"){
     cf.obj$dat.combat <- cf.obj$dat.combat %>%
       mutate(across(c(l), \(x) un_log(x)))
   }
