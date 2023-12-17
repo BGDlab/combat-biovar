da95950968216f8a8b733312ed41ef71d8854d2f
Margaret Gardner <mgardner457@gmail.com>
2023-12-17 12:58:45 -0500

    updating paths for lifespan analyses

--- cubic_scripts/qsub_centiles.sh
+++ cubic_scripts/qsub_centiles.sh
@@ -7,15 +7,15 @@
 img=/cbica/home/gardnerm/software/containers/r_gamlss_0.0.1.sif #singularity image
 base=/cbica/home/gardnerm/combat-biovar #base path (cubic) - also path that output csvs will be saved to
 r_script=$base/cubic_scripts/R_scripts/fit_centiles.R #path to .R script
-csv_path=$base/data/ukb_to_model #path to data csvs
-mod_path=$base/ukb_basic/gamlss_objs #path to gamlss .rds objs
+csv_path=$base/data/lifespan #path to data csvs
+mod_path=$base/lifespan/gamlss_objs #path to gamlss .rds objs
 #######################################################################################
 cd $base/cubic_scripts #to source functions correctly
 #######################################################################################
 # MAKE DIRECTORY
 
 #qsub script & outputs
-bash_dir=$base/ukb_basic/centile_qsubs
+bash_dir=$base/lifespan/centile_qsubs
 if ! [ -d $bash_dir ]
 	then
 	mkdir $bash_dir
@@ -26,7 +26,7 @@ if ! [ -d $bash_dir ]
 #######################################################################################
 #LIST POSSIBLE CONFIGS, INCLUDING NAME OF ORIGINAL RAW DATA
 #be sure to include "data" at the end of the string so refA options aren't confused
-config_list="cf_data cf.lm_data cf.gam_data cf.gamlss_data #cf.lm_refA_data cf.gam_refA_data cf.gamlss_refA_data ukb_CN_data"
+config_list="cf.gam cf.gamlss" #cf_data cf.lm_data cf.gam_data cf.gamlss_data #cf.lm_refA_data cf.gam_refA_data cf.gamlss_refA_data ukb_CN_data
 #######################################################################################
 for config in $config_list
 do
@@ -35,7 +35,7 @@ do
 	bash_script=$bash_dir/${config}_cent.sh
 	touch $bash_script
 	
-	echo "singularity run --cleanenv $img Rscript --save $r_script $csv_path $mod_path $base/ukb_basic $config" > $bash_script
+	echo "singularity run --cleanenv $img Rscript --save $r_script $csv_path $mod_path $base/lifespan $config" > $bash_script
 
 	#qsub bash script
 	qsub -N $config -o $bash_dir/${config}_out.txt -e $bash_dir/${config}_err.txt $bash_script
