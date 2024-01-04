299aad82f2f8c74983836e62ce8f4da0d0f2044b
Margaret Gardner Margaret.Gardner@Pennmedicine.upenn.edu <mgardner457@gmail.com>
2024-01-04 13:18:47 -0500

    updating gam and gamlss formulas to reflect log_age instead of age_days

--- cubic_scripts/qsub_combat_test.sh
+++ cubic_scripts/qsub_combat_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+\#!/bin/bash
 # Apply ComFam() with different arguments to specified dataset
 # test combat correction with shift
 
@@ -107,7 +107,7 @@ do
 	#COMBAT GAM
 	elif [ $config = "cf.gam" ]
 	then
-		echo "singularity run --cleanenv $img Rscript --save $cf_script $csv $batch $save_path $config $covar_list 'gam, formula = y ~ s(age_days) + sexMale + s(sex.age)'" > $bash_script
+		echo "singularity run --cleanenv $img Rscript --save $cf_script $csv $batch $save_path $config $covar_list 'gam, formula = y ~ s(log_age) + sexMale + s(sex.age)'" > $bash_script
 
 	#COMBAT GAM W/ REF SITE
 	elif [ $config = "cf.gam_refA" ]
@@ -117,7 +117,7 @@ do
 	#COMBAT GAMLSS
 	elif [ $config = "cf.gamlss" ]
 	then
-		echo "singularity run --cleanenv $img Rscript --save $cf_script $csv $batch $save_path $config $covar_list 'gamlss, formula = y ~ pb(age_days) + sexMale + pb(sex.age), sigma.formula = ~ pb(age_days) + sexMale + pb(sex.age)'" > $bash_script
+		echo "singularity run --cleanenv $img Rscript --save $cf_script $csv $batch $save_path $config $covar_list 'gamlss, formula = y ~ pb(log_age) + sexMale + pb(sex.age), sigma.formula = ~ pb(age_days) + sexMale + pb(sex.age)'" > $bash_script
 
 	#COMBAT GAMLSS W/ REF SITE
 	elif [ $config = "cf.gamlss_refA" ]
