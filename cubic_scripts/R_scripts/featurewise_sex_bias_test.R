397d9f682f1f60e97e7dd37073cb1946c15b2e0e
Margaret Gardner <mgardner457@gmail.com>
2024-01-06 20:19:33 -0500

    cleaning up rmds, prepping to run sex-bias t tests on cubic

---
+++ cubic_scripts/R_scripts/featurewise_sex_bias_test.R
@@ -0,0 +1,47 @@
+#Within each combat config, test if there are significant differences in M and F centile errors in each feature 
+
+set.seed(12345)
+
+#LOAD PACKAGES
+library(dplyr)
+library(data.table)
+
+source("R_scripts/gamlss_helper_funs.R")
+
+#pheno lists
+pheno_list <- readRDS(file="R_scripts/pheno_list.rds")
+
+#get args
+args <- commandArgs(trailingOnly = TRUE)
+data_path <- args[1] #path to csvs
+
+
+#Read in centile/z-score errors
+
+raw_files <- list.files(path = data_path, pattern = "_diffs.csv", full.names = TRUE) %>% unlist()
+
+df_list <- list() #new empty list
+for (file in raw_files) {
+  # Read each CSV file
+  data <- fread(file)
+  
+  data <- data %>%
+    mutate(dataset = gsub("_data|_data_predictions.csv|prop-|[0-9]|perm|-", "", Source_File))
+  
+  # Bind the data to the combined dataframe
+  df_list <- c(df_list, list(data))
+  assign("diffs_perm.list", df_list)
+}
+print(paste("length=", length(diffs_perm.list)))
+
+#centile t-tests
+cent.sex_t_tests_in_feat.df <- mclapply(diffs_perm.list, sex.bias.feat.t.tests, mc.preschedule = FALSE, comp_multiplier=length(diffs_perm.list), feature_list=pheno_list)
+
+#save rds
+saveRDS(cent.sex_t_tests_in_feat.df, file=paste0(data_path, "/featurewise_cent_sex_bias_tests.RDS"))
+
+#z-score t-tests
+z.sex_t_tests_in_feat.df <- mclapply(diffs_perm.list, sex.bias.feat.t.tests, mc.preschedule = FALSE, comp_multiplier=length(diffs_perm.list), feature_list=paste0(pheno_list,".z"))
+
+#save rds
+saveRDS(z.sex_t_tests_in_feat.df, file=paste0(data_path, "/featurewise_z_sex_bias_tests.RDS"))
--- cubic_scripts/pull_derivatives.sh
+++ cubic_scripts/pull_derivatives.sh
@@ -11,6 +11,7 @@ if ! [ -d $base/ukb_basic ]
 	fi
 echo "pulling from ukb_basic"
 scp -r 'gardnerm@cubic-login.uphs.upenn.edu:/cbica/home/gardnerm/combat-biovar/ukb_basic/centile_csvs/*.csv' $base/ukb_basic
+echo "pulling from ukb_basic - subject-wise"
 scp -r 'gardnerm@cubic-login.uphs.upenn.edu:/cbica/home/gardnerm/combat-biovar/ukb_basic/centile_csvs/subject-wise/*.csv' $base/ukb_basic
 
 # Replication (Permutations) #
@@ -20,6 +21,7 @@ if ! [ -d $base/ukb_perm ]
 	fi
 echo "pulling from ukb_permute"
 scp -r 'gardnerm@cubic-login.uphs.upenn.edu:/cbica/home/gardnerm/combat-biovar/ukb_permute/perm_centile_csvs/*.csv' $base/ukb_perm
+echo "pulling from ukb_permute - subject-wise"
 scp -r 'gardnerm@cubic-login.uphs.upenn.edu:/cbica/home/gardnerm/combat-biovar/ukb_permute/perm_centile_csvs/subject-wise/*.csv' $base/ukb_perm
 
 # Varying M:F Ratios (Proportions) #
@@ -29,6 +31,7 @@ if ! [ -d $base/ukb_ratio ]
 	fi
 echo "pulling from ukb_ratios"
 scp -r 'gardnerm@cubic-login.uphs.upenn.edu:/cbica/home/gardnerm/combat-biovar/ukb_ratios/perm_centile_csvs/*.csv' $base/ukb_ratio
+echo "pulling from ukb_ratios - subject-wise"
 scp -r 'gardnerm@cubic-login.uphs.upenn.edu:/cbica/home/gardnerm/combat-biovar/ukb_ratios/perm_centile_csvs/subject-wise/*.csv' $base/ukb_ratio
 
 # LBCC Lifespan #
