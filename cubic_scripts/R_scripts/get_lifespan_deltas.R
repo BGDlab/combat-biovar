b8370cdabc39f5762b69963561abbeb0cba365c0
Margaret Gardner <mgardner457@gmail.com>
2024-01-21 14:17:33 -0500

    fixes, adding script to get diffs between combat gam and combatls centiles

---
+++ cubic_scripts/R_scripts/get_lifespan_deltas.R
@@ -0,0 +1,57 @@
+# change in centile scores for subjects when calculated on data harmonized with ComBat GAM vs ComBatLS
+
+#LOAD PACKAGES
+library(data.table)
+library(tidyverse)
+library(dplyr)
+
+#get args
+args <- commandArgs(trailingOnly = TRUE)
+gam_csv_path <- as.character(args[1]) #path to combat-GAM fit centiles
+gamlss_csv_path <- as.character(args[2]) #path to combatLS fit centiles
+save_path <- as.character(args[3]) #path to save output csv
+fname_str <- as.character(args[4]) #string for saving name
+
+#LOAD DFS
+#read in data
+gam.df <- fread(gam_csv_path)
+gamlss.df <- fread(gamlss_csv_path)
+
+#get identifiers
+id_cols <- c("participant", "sex", "age_days", "site", "log_age", "sexMale", "sex.age", "fs_version")
+
+id.gam.df <- gam.df %>%
+  dplyr::select(all_of(id_cols)) %>%
+  mutate(id = row_number())
+
+id.gamlss.df <- gamlss.df %>%
+  dplyr::select(all_of(id_cols)) %>%
+  mutate(id = row_number())
+
+stopifnot(id.gam.df == id.gamlss.df) #make sure same subj. in each
+
+#drop ID cols and subtract across 
+gam.df <- gam.df %>%
+  dplyr::select(!all_of(id_cols))
+
+gamlss.df <- gamlss.df %>%
+  dplyr::select(!all_of(id_cols))
+
+#checks
+stopifnot(names(gam.df) == names(gamlss.df))
+stopifnot(nrow(gam.df) == nrow(gamlss.df))
+
+#subtract
+diff.df <- gam.df - gamlss.df
+diff.df <- diff.df %>%
+  mutate(id = row_number())
+
+#merge back
+stopifnot(nrow(diff.df) == nrow(id.gam.df))
+cf_deltas <- base::merge(id.gam.df, diff.df, by = "id") %>%
+  dplyr::select(!id)
+
+#SAVE
+fwrite(cf_deltas, paste0(save_path, "/", fname_str, "_pred_deltas.csv"))
+
+
---
+++ cubic_scripts/qsub_lbcc_cent_deltas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#see how different predicted centiles are for each subject when ComBat-GAM or ComBatLS are used to harmonize LBCC
+
+#######################################################################################
+# SET PATHS
+img=/cbica/home/gardnerm/software/containers/r_gamlss_0.0.1.sif #singularity image
+base=/cbica/home/gardnerm/combat-biovar #base path (cubic) - also path that output csvs will be saved to
+r_script=$base/cubic_scripts/R_scripts/get_lifespan_deltas.R #path to .R script
+cent_path=$base/data/lifespan/centile_csvs #path to centile csvs
+bash_dir=$base/lifespan/centile_qsubs
+
+#search string:
+config="imp-sites_log-cf"
+#######################################################################################
+cd $base/cubic_scripts #to source functions correctly
+#######################################################################################
+bash_script=$bash_dir/${config}_deltas.sh
+touch $bash_script
+
+#arg1=gam csv path, arg2=gamlss csv path, arg3=save path, arg4=search string
+echo "singularity run --cleanenv $img Rscript --save $r_script $cent_path/${config}_cf.gam_data_predictions.csv $cent_path/${config}_cf.gamlss_data_predictions.csv $cent_path $config" > $bash_script
+
+#qsub bash script
+qsub -N $config -o $bash_dir/${config}_deltas_out.txt -e $bash_dir/${config}_deltas_err.txt $bash_script
