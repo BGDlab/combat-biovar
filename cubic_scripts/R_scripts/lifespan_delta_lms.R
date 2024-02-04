316a5b1a7f3aa39eb0cfc41483d9b3bfa939a5b8
Margaret Gardner <mgardner457@gmail.com>
2024-02-04 17:08:42 -0500

    test age in years instead of days

--- cubic_scripts/R_scripts/lifespan_delta_lms.R
+++ cubic_scripts/R_scripts/lifespan_delta_lms.R
@@ -34,6 +34,10 @@ tests.df <- data.frame("term" = character(),
 #only test phenos w models that converged
 phenos_to_test <- intersect(pheno_list, names(deltas_df))
 
+#create age_years 
+deltas_df <- deltas_df %>%
+  mutate(age_yrs = age_days/365.25)
+
 for (pheno in phenos_to_test) {
   # Test Sex
   sex.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ sexMale")), data=deltas_df)) %>%
@@ -41,14 +45,14 @@ for (pheno in phenos_to_test) {
     mutate(iv = "sexMale")
   
   # Test Age
-  age.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ age_days")), data=deltas_df)) %>%
-    dplyr::filter(term =="age_days") %>%
-    mutate(iv = "age_days")
+  age.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ age_yrs")), data=deltas_df)) %>%
+    dplyr::filter(term =="age_yrs") %>%
+    mutate(iv = "age_yrs")
   
   # Test Sex*Age
-  int.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ sexMale*age_days")), data=deltas_df)) %>%
-    dplyr::filter(term =="sexMale:age_days") %>%
-    mutate(iv = "sexMale:age_days")
+  int.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ sexMale*age_yrs")), data=deltas_df)) %>%
+    dplyr::filter(term =="sexMale:age_yrs") %>%
+    mutate(iv = "sexMale:age_yrs")
   
   # Compile w/in phenotype
   stopifnot(dim(sex.df) == dim(age.df) & dim(age.df) == dim(int.df))
@@ -92,14 +96,14 @@ for (pheno in phenos_to_test.z) {
     mutate(iv = "sexMale")
   
   # Test Age
-  age.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ age_days")), data=deltas_df)) %>%
-    dplyr::filter(term =="age_days") %>%
-    mutate(iv = "age_days")
+  age.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ age_yrs")), data=deltas_df)) %>%
+    dplyr::filter(term =="age_yrs") %>%
+    mutate(iv = "age_yrs")
   
   # Test Sex*Age
-  int.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ sexMale*age_days")), data=deltas_df)) %>%
-    dplyr::filter(term =="sexMale:age_days") %>%
-    mutate(iv = "sexMale:age_days")
+  int.df <- tidy(lm(formula=as.formula(paste0("delta.", pheno, " ~ sexMale*age_yrs")), data=deltas_df)) %>%
+    dplyr::filter(term =="sexMale:age_yrs") %>%
+    mutate(iv = "sexMale:age_yrs")
   
   # Compile w/in phenotype
   stopifnot(dim(sex.df) == dim(age.df) & dim(age.df) == dim(int.df))
