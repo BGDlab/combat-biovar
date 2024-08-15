b346d046a3ee265ad367f44283909fb97bba4d19
Margaret Gardner <mgardner457@gmail.com>
2024-08-15 10:41:37 -0400

    linting

--- cubic_scripts/R_scripts/ranked_welch_tests.R
+++ cubic_scripts/R_scripts/ranked_welch_tests.R
@@ -1,49 +1,51 @@
-#load libraries
+# load libraries
 library(stats)
 
-#contains 3 functions for running welch's t-test on data rankings (accounting for both skew and unequal variances - Zimmerman & Zumbo, 1993)
+# contains 3 functions for running welch's t-test on data rankings
+# (accounting for both skew and unequal variances - Zimmerman & Zumbo, 1993)
 
 # rank.welch.t.test() - analogous to t.test()
 # pairwise.rank.welch.t.test() - analogous to pairwise.t.test()
 # pairwise.rank.maxgrp() - also returns group with larger ranks
 
-rank.welch.t.test <- function (x, ...) {
+rank.welch.t.test <- function(x, ...) {
   UseMethod("rank.welch.t.test", x)
 }
 
 ## Default S3 method:
 rank.welch.t.test.default <- function(x_list, y_list,
-                              alternative = c("two.sided", "less", "greater"),
-                              mu = 0, paired = FALSE, conf.level = 0.95, ...){
-  #rank pooled vals
+                                      alternative = c("two.sided", "less", "greater"),
+                                      mu = 0, paired = FALSE, conf.level = 0.95, ...) {
+  # rank pooled vals
   pooled <- c(x_list, y_list)
-  pooled_r <- rank(pooled, na.last=NA) #remove NAs
-  
+  pooled_r <- rank(pooled, na.last = NA) # remove NAs
+
   # Split the rankings back into the original lists
   x_ranks <- pooled_r[seq_along(x_list)]
   y_ranks <- pooled_r[seq_along(y_list) + length(x_list)]
-  
-  #kill if ranks are wrong length
+
+  # kill if ranks are wrong length
   stopifnot(length(x_ranks) == length(x_list))
   stopifnot(length(y_ranks) == length(y_list))
-  
-  #conduct t.test
-  welch.test <- t.test(x = x_ranks, y = y_ranks, var.equal = FALSE, 
-                       alternative = alternative, mu = mu, paired = paired, conf.level = conf.level)
-  
+
+  # conduct t.test
+  welch.test <- t.test(
+    x = x_ranks, y = y_ranks, var.equal = FALSE,
+    alternative = alternative, mu = mu, paired = paired, conf.level = conf.level
+  )
+
   return(welch.test)
 }
 
 
 ## S3 method for class 'formula' (based on t.test.formula())
-rank.welch.t.test.formula <- function(formula, data, subset, na.action, ...)
-{
-  if(missing(formula)
-     || (length(formula) != 3L)
-     || (length(attr(terms(formula[-2L]), "term.labels")) != 1L))
+rank.welch.t.test.formula <- function(formula, data, subset, na.action, ...) {
+  if (missing(formula) ||
+    (length(formula) != 3L) ||
+    (length(attr(terms(formula[-2L]), "term.labels")) != 1L))
     stop("'formula' missing or incorrect")
   m <- match.call(expand.dots = FALSE)
-  if(is.matrix(eval(m$data, parent.frame())))
+  if (is.matrix(eval(m$data, parent.frame())))
     m$data <- as.data.frame(data)
   ## need stats:: for non-standard evaluation
   m[[1L]] <- quote(stats::model.frame)
@@ -53,125 +55,135 @@ rank.welch.t.test.formula <- function(formula, data, subset, na.action, ...)
   names(mf) <- NULL
   response <- attr(attr(mf, "terms"), "response")
   # rank values
-  mf[[response]] <- rank(mf[[response]], na.last=NA)
+  mf[[response]] <- rank(mf[[response]], na.last = NA)
   # group
   g <- factor(mf[[-response]])
-  if(nlevels(g) != 2L)
+  if (nlevels(g) != 2L)
     stop("grouping factor must have exactly 2 levels")
   DATA <- setNames(split(mf[[response]], g), c("x", "y"))
   y <- do.call("t.test", c(DATA, var.equal = FALSE, list(...)))
   y$data.name <- DNAME
-  if(length(y$estimate) == 2L)
+  if (length(y$estimate) == 2L)
     names(y$estimate) <- paste("mean in group", levels(g))
   y
 }
 
 pairwise.rank.welch.t.test <- function(x, g, p.adjust.method = p.adjust.methods,
-                                       paired = FALSE, alternative = c("two.sided", "less", "greater"),
-                                       ...){
-  
+                                       paired = FALSE,
+                                       alternative = c("two.sided", "less", "greater"),
+                                       ...) {
   # code based heavily on pairwise.t.test() and pairwise.wilcox.test()
-  
+
   ## parse args
   p.adjust.method <- match.arg(p.adjust.method)
   DNAME <- paste(deparse(substitute(x)), "and", deparse(substitute(g)))
   g <- factor(g)
   METHOD <- if (paired) "Wilcoxon signed rank test"
   else "Wilcoxon rank sum test"
-  
+
   ## comp matrix
   compare.levels <- function(i, j) {
-    
     ## get vals
     xi <- x[as.integer(g) == i]
     xj <- x[as.integer(g) == j]
-    
-    #rank pooled vals
+
+    # rank pooled vals
     pooled <- c(xi, xj)
-    pooled_r <- rank(pooled, na.last=NA)
-    
+    pooled_r <- rank(pooled, na.last = NA)
+
     ## split rankings back into the original lists
     x_ranks <- pooled_r[seq_along(xi)]
     y_ranks <- pooled_r[seq_along(xj) + length(xi)]
-    
+
     ## test
-    t.test(x_ranks, y_ranks, paired=paired, var.equal = FALSE, alternative = alternative, ...)$p.value
+    t.test(x_ranks, y_ranks, paired = paired, var.equal = FALSE, alternative = alternative, ...)$p.value
   }
-  
+
   ## compile results
   PVAL <- pairwise.table(compare.levels, levels(g), p.adjust.method)
-  
-  #find which site has larger estimate 
+
+  # find which site has larger estimate
   ## comp matrix
   compare.levels.est <- function(i, j) {
-    
     ## get vals
     xi <- x[as.integer(g) == i]
     xj <- x[as.integer(g) == j]
-    
-    #rank pooled vals
+
+    # rank pooled vals
     pooled <- c(xi, xj)
-    pooled_r <- rank(pooled, na.last=NA)
-    
+    pooled_r <- rank(pooled, na.last = NA)
+
     ## split rankings back into the original lists
     x_ranks <- pooled_r[seq_along(xi)]
     y_ranks <- pooled_r[seq_along(xj) + length(xi)]
-    
+
     ## get est
-    est <- broom::tidy(t.test(x_ranks, y_ranks, paired=paired, var.equal = FALSE, alternative = alternative, ...))$estimate
-    
+    est <- broom::tidy(t.test(x_ranks, y_ranks,
+      paired = paired,
+      var.equal = FALSE,
+      alternative = alternative, ...
+    ))$estimate
+
     bigger_group <- ifelse(est > 0, i, j)
+    return(bigger_group)
   }
   # (estimate = estimate1-estimate2) ~ positive -> levels(g)[1], negative -> levels(g)[2]
   big_group <- pairwise.table(compare.levels.est, levels(g), p.adjust.method)
   est_matrix <- matrix(as.list(levels(g))[big_group], ncol = ncol(big_group), dimnames = dimnames(big_group))
-  
-  ans <- list(method = METHOD, data.name = DNAME,
-              p.value = PVAL, p.adjust.method=p.adjust.method, larger_group = est_matrix)
+
+  ans <- list(
+    method = METHOD, data.name = DNAME,
+    p.value = PVAL, p.adjust.method = p.adjust.method,
+    larger_group = est_matrix
+  )
   class(ans) <- "pairwise.htest" # prevents larger_group from displaying but enables tidy()
   ans
 }
 
-#making new fun that will return the level with the greater estimated ranks in each pairwise comparison
+# making new fun that will return the level with the greater estimated ranks in each pairwise comparison
 pairwise.rank.maxgrp <- function(x, g, p.adjust.method = p.adjust.method,
-                                       paired = FALSE, alternative = c("two.sided", "less", "greater"),
-                                       ...){
-  
+                                 paired = FALSE, alternative = c("two.sided", "less", "greater"),
+                                 ...) {
   ## parse args
   p.adjust.method <- match.arg(p.adjust.method)
   DNAME <- paste(deparse(substitute(x)), "and", deparse(substitute(g)))
   g <- factor(g)
   METHOD <- if (paired) "return group with larger ranks estimated by Wilcoxon signed rank test"
   else "return group with larger ranks estimated by Wilcoxon rank sum test"
-  
 
-  #find which site has larger estimate 
+
+  # find which site has larger estimate
   ## comp matrix
   compare.levels.est <- function(i, j) {
-    
     ## get vals
     xi <- x[as.integer(g) == i]
     xj <- x[as.integer(g) == j]
-    
-    #rank pooled vals
+
+    # rank pooled vals
     pooled <- c(xi, xj)
-    pooled_r <- rank(pooled, na.last=NA)
-    
+    pooled_r <- rank(pooled, na.last = NA)
+
     ## split rankings back into the original lists
     x_ranks <- pooled_r[seq_along(xi)]
     y_ranks <- pooled_r[seq_along(xj) + length(xi)]
-    
+
     ## get est
-    est <- broom::tidy(t.test(x_ranks, y_ranks, paired=paired, var.equal = FALSE, alternative = alternative, ...))$estimate
-    
-    bigger_group <- ifelse(est > 0, i, j) #i, j
+    est <- broom::tidy(t.test(x_ranks, y_ranks,
+      paired = paired,
+      var.equal = FALSE,
+      alternative = alternative, ...
+    ))$estimate
+
+    bigger_group <- ifelse(est > 0, i, j) # i, j
+    return(bigger_group)
   }
   # (estimate = estimate1-estimate2) ~ positive -> levels(g)[1], negative -> levels(g)[2]
   big_group <- pairwise.table(compare.levels.est, levels(g), p.adjust.method)
-  # est_matrix <- matrix(as.list(levels(g))[big_group], ncol = ncol(big_group), dimnames = dimnames(big_group))
-  
-  ans <- list(method = METHOD, data.name = DNAME,
-              p.value = big_group, p.adjust.method=p.adjust.method) #estimate column needs to be mislabeld so that tidy() work
+
+  ans <- list(
+    method = METHOD, data.name = DNAME,
+    p.value = big_group, p.adjust.method = p.adjust.method
+  ) # estimate column needs to be mislabeld so that tidy() work
   class(ans) <- "pairwise.htest"
   ans
 }
--- cubic_scripts/R_scripts/stats_tests.R
+++ cubic_scripts/R_scripts/stats_tests.R
@@ -1,4 +1,4 @@
-#load libraries
+# load libraries
 library(dplyr)
 library(ggplot2)
 library(data.table)
@@ -13,43 +13,60 @@ devtools::source_url("https://raw.githubusercontent.com/BGDlab/combat-biovar/mai
 ## STATS
 ##########
 
-#centile.t.tests()
-#Pairwise T tests: within each feature, run paired t tests (welch's test on ranks) pairwise on each possible pairing of combat configs and return as a single dataframe, with FDR correction for all feature + cf pair combos. Currently runs on magnitude (abs val) of centile diffs, but can be run on other metrics by redefining `feature_list` Recommended for use with mclapply across list of dataframes. Optional argument to correct for more comparisons (e.g. fdr-correct across an entire list of dataframes) using `comp_multiplier` arg.
+# centile.t.tests()
+# Pairwise T tests: within each feature, run paired t tests (welch's test on ranks) pairwise 
+# on each possible pairing of combat configs and return as a single dataframe, 
+# with FDR correction for all feature + cf pair combos. 
+# Currently runs on magnitude (abs val) of centile diffs, but can be run on other metrics by redefining `feature_list`.
+# Recommended for use with mclapply across list of dataframes. 
+# Optional argument to correct for more comparisons (e.g. fdr-correct across an 
+# entire list of dataframes) using `comp_multiplier` arg.
 
 
-centile.t.tests <- function(df, feature_list, comp_multiplier=1){
-  #initialize empty dfs to store outputs
-  t.df <- data.frame("pheno" = character(),
-                     "group1" = character(),
-                     "group2" = character(),
-                     "p.value" = double())
-  #run tests
+centile.t.tests <- function(df, feature_list, comp_multiplier = 1) {
+  # initialize empty dfs to store outputs
+  t.df <- data.frame(
+    "pheno" = character(),
+    "group1" = character(),
+    "group2" = character(),
+    "p.value" = double()
+  )
+  # run tests
   attach(df)
   for (pheno in feature_list) {
-    df.pairwise.t <- tidy(pairwise.rank.welch.t.test(x=df[[pheno]], g=df[["dataset"]], paired = TRUE, p.adj = "none"))
+    df.pairwise.t <- tidy(pairwise.rank.welch.t.test(x = df[[pheno]], 
+                                                     g = df[["dataset"]], 
+                                                     paired = TRUE, 
+                                                     p.adj = "none"))
     df.pairwise.t$pheno <- pheno
     t.df <- rbind(t.df, df.pairwise.t)
   }
   n_comp <- nrow(t.df)
 
-  #apply fdr correction
+  # apply fdr correction
   t.df <- t.df %>%
-    dplyr::mutate(p.val_fdr = p.adjust(p.value, method="fdr", n = (n_comp*comp_multiplier)),
-           sig_fdr = case_when(p.val_fdr < 0.05 ~ TRUE,
-                               p.val_fdr >= 0.05 ~ FALSE),
-           pheno = sub("abs.diff_", "", pheno)) %>%
+    dplyr::mutate(
+      p.val_fdr = p.adjust(p.value, method = "fdr", n = (n_comp * comp_multiplier)),
+      sig_fdr = case_when(
+        p.val_fdr < 0.05 ~ TRUE,
+        p.val_fdr >= 0.05 ~ FALSE
+      ),
+      pheno = sub("abs.diff_", "", pheno)
+    ) %>%
     unite(comp, c("group1", "group2")) %>%
-   dplyr::mutate(comp = as.factor(comp))
+    dplyr::mutate(comp = as.factor(comp))
 
   t.df.sum <- t.df %>%
     group_by(comp) %>%
-    dplyr::summarize(n_sig_regions = sum(sig_fdr),
-              min_pval = min(p.val_fdr),
-              max_pval = max(p.val_fdr)) %>%
+    dplyr::summarize(
+      n_sig_regions = sum(sig_fdr),
+      min_pval = min(p.val_fdr),
+      max_pval = max(p.val_fdr)
+    ) %>%
     ungroup() %>%
     as.data.frame()
   return(t.df.sum)
-  #detach()
+  # detach()
 }
 
 ### EXAMPLE:
@@ -57,195 +74,208 @@ centile.t.tests <- function(df, feature_list, comp_multiplier=1){
 # names(perm_t.tests) <- n_perm_list
 # cent_t.tests.p <- bind_rows(perm_t.tests, .id = "column_label")
 
-#centile.t.tests.full_result()
-#same as centile t-test but returns results for each feature instead of summarizing w/in each comparison
+# centile.t.tests.full_result()
+# same as centile t-test but returns results for each feature instead of summarizing w/in each comparison
 
-centile.t.tests.full_result <- function(df, feature_list, comp_multiplier=1){
-  #initialize empty dfs to store outputs
-  t.df <- data.frame("pheno" = character(),
-                     "group1" = character(),
-                     "group2" = character(),
-                     "p.value" = double())
-  #run tests
+centile.t.tests.full_result <- function(df, feature_list, comp_multiplier = 1) {
+  # initialize empty dfs to store outputs
+  t.df <- data.frame(
+    "pheno" = character(),
+    "group1" = character(),
+    "group2" = character(),
+    "p.value" = double()
+  )
+  # run tests
   attach(df)
   for (pheno in feature_list) {
-    df.pairwise.t <- tidy(pairwise.rank.welch.t.test(x=df[[pheno]], g=df[["dataset"]], paired = TRUE, p.adj = "none"))
-    # print(df.pairwise.t)
-    
-    #also find bigger group
-    df.pairwise.est <- tidy(pairwise.rank.maxgrp(x=df[[pheno]], g=df[["dataset"]], paired = TRUE, p.adj = "none"))
+    df.pairwise.t <- tidy(pairwise.rank.welch.t.test(x = df[[pheno]],
+                                                     g = df[["dataset"]],
+                                                     paired = TRUE,
+                                                     p.adj = "none"))
+
+    # also find bigger group
+    df.pairwise.est <- tidy(pairwise.rank.maxgrp(x = df[[pheno]],
+                                                 g = df[["dataset"]],
+                                                 paired = TRUE,
+                                                 p.adj = "none"))
     df.pairwise.est <- df.pairwise.est %>%
-      rename(bigger_group = p.value) #fix name
+      rename(bigger_group = p.value) # fix name
     # print(df.pairwise.est)
-    
-    #join
+
+    # join
     df.pairwise.t2 <- left_join(df.pairwise.t, df.pairwise.est)
-    
+
     stopifnot(nrow(df.pairwise.t2) == nrow(df.pairwise.t))
-    
+
     df.pairwise.t2$pheno <- pheno
     t.df <- rbind(t.df, df.pairwise.t2)
   }
   n_comp <- nrow(t.df)
-  
+
   dataset_vec <- levels(as.factor(df$dataset))
-  
-  #apply fdr correction
+
+  # apply fdr correction
   t.df <- t.df %>%
-    dplyr::mutate(p.val_fdr = p.adjust(p.value, method="fdr", n = (n_comp*comp_multiplier)),
-                  sig_fdr = case_when(p.val_fdr < 0.05 ~ TRUE,
-                                      p.val_fdr >= 0.05 ~ FALSE),
-                  pheno = sub("abs.diff_", "", pheno),
-                  bigger_group = dataset_vec[bigger_group]) %>%
+    dplyr::mutate(
+      p.val_fdr = p.adjust(p.value, method = "fdr", n = (n_comp * comp_multiplier)),
+      sig_fdr = case_when(
+        p.val_fdr < 0.05 ~ TRUE,
+        p.val_fdr >= 0.05 ~ FALSE
+      ),
+      pheno = sub("abs.diff_", "", pheno),
+      bigger_group = dataset_vec[bigger_group]
+    ) %>%
     unite(comp, c("group1", "group2")) %>%
     dplyr::mutate(comp = as.factor(comp))
-  
+
   return(t.df)
-  #detach()
+  # detach()
 }
 
-# tidy(rank.welch.t.test.formula(formula=mean_cent_abs.diff ~ sex, data= ratio_subj_list[[1]], paired = FALSE, p.adj = "none"))
-# rank.welch.t.test.formula(formula=mean_cent_abs.diff ~ sex, data= ratio_subj_list[[1]], paired = FALSE, p.adj = "none")
+# use: tidy(rank.welch.t.test.formula(formula=mean_cent_abs.diff ~ sex, data= ratio_subj_list[[1]], paired = FALSE, p.adj = "none"))
+# use: rank.welch.t.test.formula(formula=mean_cent_abs.diff ~ sex, data= ratio_subj_list[[1]], paired = FALSE, p.adj = "none")
 
-#sex.bias.feat.t.tests()
-#modifying to test for sex-biases w/in feature rather than mean centiles. 
+# sex.bias.feat.t.tests()
+# modifying to test for sex-biases w/in feature rather than mean centiles.
 # optional ID_col so fun will retain prop or perm as necessary when lapplying across a list of dfs
 
-sex.bias.feat.t.tests <- function(df, feature_list, comp_multiplier=1, ID_col=NA){
-  
-  #initialize empty df to store outputs
-  t.df <- data.frame("dataset" = character(),
-                     "pheno" = character(),
-                     "p.value" = double(),
-                     "sex_diff" = double(),
-                     "t.stat" = double(),
-                     "df" = double())
+sex.bias.feat.t.tests <- function(df, feature_list, comp_multiplier = 1, ID_col = NA) {
+  # initialize empty df to store outputs
+  t.df <- data.frame(
+    "dataset" = character(),
+    "pheno" = character(),
+    "p.value" = double(),
+    "sex_diff" = double(),
+    "t.stat" = double(),
+    "df" = double()
+  )
   attach(df)
   for (pheno in feature_list) {
-    #print(paste("t-test:", pheno))
-    
-    #conduct t test
+
+    # conduct t test
     df.sex.t <- df %>%
       group_by(dataset) %>%
-      summarise(p.value = tidy(rank.welch.t.test.formula(formula=as.formula(paste(pheno, "~ sex")), paired = FALSE, p.adj = "none"))$p.value,
-                sex_diff = tidy(rank.welch.t.test.formula(formula=as.formula(paste(pheno, "~ sex")), paired = FALSE, p.adj = "none"))$estimate,
-                t.stat = tidy(rank.welch.t.test.formula(formula=as.formula(paste(pheno, "~ sex")), paired = FALSE, p.adj = "none"))$statistic,
-                df = tidy(rank.welch.t.test.formula(formula=as.formula(paste(pheno, "~ sex")), paired = FALSE, p.adj = "none"))$parameter) %>%
+      summarise(
+        p.value = tidy(rank.welch.t.test.formula(formula = as.formula(paste(pheno, "~ sex")),
+                                                 paired = FALSE, p.adj = "none"))$p.value,
+        sex_diff = tidy(rank.welch.t.test.formula(formula = as.formula(paste(pheno, "~ sex")),
+                                                  paired = FALSE, p.adj = "none"))$estimate,
+        t.stat = tidy(rank.welch.t.test.formula(formula = as.formula(paste(pheno, "~ sex")),
+                                                paired = FALSE, p.adj = "none"))$statistic,
+        df = tidy(rank.welch.t.test.formula(formula = as.formula(paste(pheno, "~ sex")),
+                                            paired = FALSE, p.adj = "none"))$parameter
+      ) %>%
       ungroup()
-    
+
     df.sex.t$pheno <- sub("diff_", "", pheno)
-    
-    #append to full df
+
+    # append to full df
     t.df <- rbind(t.df, df.sex.t)
   }
   detach(df)
-  
-  #also get medians for m and f
+
+  # also get medians for m and f
   med_df <- df %>%
     dplyr::group_by(dataset, sex) %>%
-    summarise_at(feature_list, median, na.rm=TRUE) %>%
+    summarise_at(feature_list, median, na.rm = TRUE) %>%
     ungroup() %>%
-    pivot_longer(cols = starts_with("diff_"), names_to="pheno", names_prefix="diff_") %>%
-    pivot_wider(names_from = sex, values_from=value, names_glue = "med_{sex}") %>%
+    pivot_longer(cols = starts_with("diff_"), names_to = "pheno", names_prefix = "diff_") %>%
+    pivot_wider(names_from = sex, values_from = value, names_glue = "med_{sex}") %>%
     mutate(median_sex_diff = (med_Male - med_Female)) %>%
     ungroup()
-  
-  #also get max for m and f
+
+  # also get max for m and f
   max_df <- df %>%
     dplyr::group_by(dataset, sex) %>%
-    summarise_at(feature_list, max, na.rm=TRUE) %>%
+    summarise_at(feature_list, max, na.rm = TRUE) %>%
     ungroup() %>%
-    pivot_longer(cols = starts_with("diff_"), names_to="pheno", names_prefix="diff_") %>%
-    pivot_wider(names_from = sex, values_from=value, names_glue = "max_{sex}") %>%
+    pivot_longer(cols = starts_with("diff_"), names_to = "pheno", names_prefix = "diff_") %>%
+    pivot_wider(names_from = sex, values_from = value, names_glue = "max_{sex}") %>%
     mutate(max_sex_diff = (max_Male - max_Female)) %>%
     ungroup()
-  
-  #combine feature dfs
-  result_df <- Reduce(left_join, c(list(t.df), list(med_df), list(max_df)))  
-  
-  #apply fdr correction
-  n_comp=length(unique(df$dataset))*length(feature_list)
+
+  # combine feature dfs
+  result_df <- Reduce(left_join, c(list(t.df), list(med_df), list(max_df)))
+
+  # apply fdr correction
+  n_comp <- length(unique(df$dataset)) * length(feature_list)
   result_df <- result_df %>%
-    dplyr::mutate(p.val_fdr = p.adjust(p.value, method="fdr", n = (n_comp*comp_multiplier)), #use comp_multiplier if using fun with lapply
-                  sig_fdr = case_when(p.val_fdr < 0.05 ~ TRUE,
-                                      p.val_fdr >= 0.05 ~ FALSE),
-                  dataset=as.factor(dataset))
-  
-  #add back source file info in ID_col if needed
+    dplyr::mutate(
+      p.val_fdr = p.adjust(p.value, method = "fdr",
+                           n = (n_comp * comp_multiplier)),
+                          # use comp_multiplier if using fun with lapply
+      sig_fdr = case_when(
+        p.val_fdr < 0.05 ~ TRUE,
+        p.val_fdr >= 0.05 ~ FALSE
+      ),
+      dataset = as.factor(dataset)
+    )
+
+  # add back source file info in ID_col if needed
   if (!is.na(ID_col)) {
     print(paste("ID col vals:", unique(df[[ID_col]])))
     stopifnot(length(unique(df[[ID_col]])) == 1)
     result_df[[ID_col]] <- unique(df[[ID_col]])
   }
-  # #summarize
-  # df.sex.t.sum <- df.sex.t %>%
-  #   group_by(dataset) %>%
-  #   dplyr::summarize(n_sig_regions = sum(sig_fdr),
-  #             min_pval = min(p.val_fdr),
-  #             max_pval = max(p.val_fdr)) %>%
-  #   ungroup() %>%
-  #   as.data.frame()
-  
-  return(result_df)
-  
-}
-# sex.bias.feat.t.tests(diffs.df, feature_list = global.diff)
 
-sex.bias.feat.wilcox.tests <- function(df, feature_list, comp_multiplier=1){
-  
-  #initialize empty df to store outputs
-  t.df <- data.frame("dataset" = character(),
-                     "pheno" = character(),
-                     "p.value" = double(),
-                     "w.stat" = double())
+  return(result_df)
+}
+# use: sex.bias.feat.t.tests(diffs.df, feature_list = global.diff)
+
+sex.bias.feat.wilcox.tests <- function(df, feature_list, comp_multiplier = 1) {
+  # initialize empty df to store outputs
+  t.df <- data.frame(
+    "dataset" = character(),
+    "pheno" = character(),
+    "p.value" = double(),
+    "w.stat" = double()
+  )
   attach(df)
   for (pheno in feature_list) {
-    #print(paste("t-test:", pheno))
-    
-    #conduct t test
+
+    # conduct t test
     df.sex.t <- df %>%
       group_by(dataset) %>%
-      summarise(p.value = tidy(wilcox.test(formula=as.formula(paste(pheno, "~ sex")), paired = FALSE, p.adj = "none"))$p.value,
-                w.stat = tidy(wilcox.test(formula=as.formula(paste(pheno, "~ sex")), paired = FALSE, p.adj = "none"))$statistic) %>%
+      summarise(
+        p.value = tidy(wilcox.test(formula = as.formula(paste(pheno, "~ sex")),
+                                   paired = FALSE, p.adj = "none"))$p.value,
+        w.stat = tidy(wilcox.test(formula = as.formula(paste(pheno, "~ sex")),
+                                  paired = FALSE, p.adj = "none"))$statistic
+      ) %>%
       ungroup()
-    
+
     df.sex.t$pheno <- sub("diff_", "", pheno)
-    
-    #append to full df
+
+    # append to full df
     t.df <- rbind(t.df, df.sex.t)
   }
   detach(df)
-  
-  #also get medians for m and f
+
+  # also get medians for m and f
   med_df <- df %>%
     dplyr::group_by(dataset, sex) %>%
     summarise_at(feature_list, median) %>%
     ungroup() %>%
-    pivot_longer(cols = starts_with("diff_"), names_to="pheno", names_prefix="diff_") %>%
-    pivot_wider(names_from = sex, values_from=value, names_glue = "med_{sex}") %>%
+    pivot_longer(cols = starts_with("diff_"), names_to = "pheno", names_prefix = "diff_") %>%
+    pivot_wider(names_from = sex, values_from = value, names_glue = "med_{sex}") %>%
     mutate(median_sex_diff = (med_Male - med_Female)) %>%
     ungroup()
-  
-  #combine feature dfs
-  result_df <- left_join(t.df, med_df)  
-  
-  #apply fdr correction
-  n_comp=length(unique(df$dataset))*length(feature_list)
+
+  # combine feature dfs
+  result_df <- left_join(t.df, med_df)
+
+  # apply fdr correction
+  n_comp <- length(unique(df$dataset)) * length(feature_list)
   result_df <- result_df %>%
-    dplyr::mutate(p.val_fdr = p.adjust(p.value, method="fdr", n = (n_comp*comp_multiplier)), #use comp_multiplier if using fun with lapply
-                  sig_fdr = case_when(p.val_fdr < 0.05 ~ TRUE,
-                                      p.val_fdr >= 0.05 ~ FALSE),
-                  dataset=as.factor(dataset))
-  
-  # #summarize
-  # df.sex.t.sum <- df.sex.t %>%
-  #   group_by(dataset) %>%
-  #   dplyr::summarize(n_sig_regions = sum(sig_fdr),
-  #             min_pval = min(p.val_fdr),
-  #             max_pval = max(p.val_fdr)) %>%
-  #   ungroup() %>%
-  #   as.data.frame()
-  
+    dplyr::mutate(
+      p.val_fdr = p.adjust(p.value, method = "fdr", n = (n_comp * comp_multiplier)),
+      # use comp_multiplier if using fun with lapply
+      sig_fdr = case_when(
+        p.val_fdr < 0.05 ~ TRUE,
+        p.val_fdr >= 0.05 ~ FALSE
+      ),
+      dataset = as.factor(dataset)
+    )
+
   return(result_df)
-  
 }
