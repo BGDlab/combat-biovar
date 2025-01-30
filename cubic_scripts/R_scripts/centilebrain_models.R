0d79a05741f087da14f358fd10bbeaaaa774a8f1
Margaret Gardner <mgardner457@gmail.com>
2025-01-29 19:02:11 -0500

    refit lms mod with gamlss to get centiles

--- cubic_scripts/R_scripts/centilebrain_models.R
+++ cubic_scripts/R_scripts/centilebrain_models.R
@@ -14,20 +14,38 @@ df <- fread(args[1], stringsAsFactors = TRUE, na.strings = "")
 pheno <- as.character(args[2])
 save_path <- args[3]
 
-#FIT GAMLSS
+#FIT LMS
 model_formula <- paste0("lms(", pheno, ", age_days, method.pb='GAIC', data=df, k=5, calibration=FALSE)")
 
 mod <- eval(parse(text=model_formula))
+
+#FIT GAMLSS
+fam <- mod$family[1]
+print(paste("fitting", pheno, "with", fam))
+
+if ("tau" %in% mod[[2]]){
+  gamlss_formula <-paste("gamlss(formula =", pheno, "~ pb(age_days, method='GAIC', k=5),",
+                         "sigma.formula = ~ pb(age_days, method='GAIC', k=5),",
+                         "nu.formula = ~ pb(age_days, method='GAIC', k=5),",
+                         "tau.formula = ~ pb(age_days, method='GAIC', k=5),",
+                         "family =", fam, ", data=df, trace = FALSE)")
+} else {
+  gamlss_formula <-paste("gamlss(formula =", pheno, "~ pb(age_days, method='GAIC', k=5),",
+                         "sigma.formula = ~ pb(age_days, method='GAIC', k=5),",
+                         "nu.formula = ~ pb(age_days, method='GAIC', k=5),", 
+                         "family =", fam, ", data=df, trace = FALSE)")
+}
+
+base_mod <- eval(parse(text = gamlss_formula))
 
 #SAVE
 csv_name <- sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(as.character(args[1])))
 csv_rename <- gsub("_", "-", csv_name)
 
-saveRDS(mod,paste0(save_path, "/", pheno, "_", csv_rename, "_cb_mod.rds"))
+saveRDS(base_mod,paste0(save_path, "/", pheno, "_", csv_rename, "_cb_mod.rds"))
 
 #ALSO FIT MOD WITH SITE EFFECTS
 #easier to match mod with df
-fam <- mod$family[1]
 
 print(paste("fitting", pheno, "with", fam, "family to get study terms"))
 
